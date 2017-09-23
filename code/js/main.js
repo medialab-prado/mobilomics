be290e1b545d4f2fc7acce428d12dd220344ada7
marmarto <meryalca@gmail.com>
2017-09-23 18:13:14 +0200

    geojson de redes sociales por hora y modificación del mapa para ver primeras horas del día en distintos colores

--- code/js/main.js
+++ code/js/main.js
@@ -1,3 +1,4 @@
+
 var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -16,6 +17,7 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
     "Streets": streets
   };
 
+
   L.control.layers(baseLayers).addTo(mymap)
 
 
@@ -26,10 +28,12 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
     weight: 1,
     opacity: 1,
     fillOpacity: 0.8
-  };
+};
 
 
-
+function onEachFeature( feature, layer) {
+  layer.bindPopup(feature.properties.hour);
+}
 
   //Add a layer for markerClusterGroup
   var markers=L.markerClusterGroup();
@@ -37,6 +41,28 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
 
 
   var twitterLayer = L.geoJson(geojsonFeature);
+
+  /*var traficoLayer = L.geoJson(int_loc, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(feature.properties.intensidad);
+    }
+  });*/
+  var socialLayer = L.geoJson(socialFeature, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    style: function (feature) {
+      switch (feature.properties.hour) {
+        case 4: return { radius: 8,fillColor: "#990000",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8};
+        case 5: return { radius: 8,fillColor: "#000099",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8};
+        case 6: return { radius: 8,fillColor: "#006600",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8};
+        case 7: return { radius: 8,fillColor: "#ff0000",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8};
+        case 8: return { radius: 8,fillColor: "#990099",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8};
+        case 9: return { radius: 8,fillColor: "#ff0099",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8};
+      }
+    }
+  });
+  var trafficLayer = L.geoJson(trafficFeature);
 
   /*var geojson = L.geoJson(geojsonSample, {
     pointToLayer: function (feature, latlng) {
@@ -53,7 +79,7 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
      }
    }).addTo(mymap);*/
     //Add geoJsonLayer to markercluster group
-    markers.addLayer(twitterLayer);
+    markers.addLayer(socialLayer);
     //ADD the markercluster group to the map
     mymap.addLayer(markers);
     //mymap.fitBounds(markers.getBounds());
